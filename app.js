78388deb49032b38cfbe7c78519ad2d42132fb10
Laura Silvey <lsilvey@gmail.com>
2017-11-20 16:43:03 -0800

    setup app and mongoose/model config

--- app.js
+++ app.js
@@ -3,16 +3,33 @@ var mongoose = require("mongoose");
 var express = require("express");
 var app = express();
 
-//change 'boiler-plate' to name of app.
-mongoose.connect("mongodb://localhost/boiler-plate", {useMongoClient: true});
+// App Config
+mongoose.connect("mongodb://localhost/portfolio", {useMongoClient: true});
 app.set("view engine", "ejs");
-//to access custom stylesheets
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Mongoose/Model Config
+var projectSchema = new mongoose.Schema({
+  image: String,
+  techList: String,
+});
 
+var Project = mongoose.model("Project", projectSchema);
+
+// RESTful Routes
 app.get("/", function(req, res){
-  res.render("home");
+  res.redirect("/projects");
+});
+
+app.get("/projects", function(req, res){
+  Project.find({}, function(err, projects){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("index", {projects});
+    }
+  });
 });
 
 app.listen(3000);
